f1b855de058581c3d71053cda83ada7b0e48e9cf
Rishat Muhamedshin <risatmuhamedsin@gmail,com>
2024-01-18 21:05:46 +0300

    Learning book Spring Action

---
+++ src/test/java/com/tacocloud/risatmuhamedsin/RisatmuhamedsinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacocloud.risatmuhamedsin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisatmuhamedsinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
